9ab2fbe4c87f842ecc9e2dbc5924481f36068054
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-04-28 10:28:39 +0700

    Use the @Register annotation to customize validation for a class

--- src/main/java/com/java/springmvc/domain/dto/RegisterDTO.java
+++ src/main/java/com/java/springmvc/domain/dto/RegisterDTO.java
@@ -1,11 +1,28 @@
 package com.java.springmvc.domain.dto;
 
+import com.java.springmvc.service.validator.Register;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Register
 public class RegisterDTO {
 
+    @NotNull
+    @Size(min = 3, message = "Invalid name, minimum of 3 characters")
     private String firstName;
+
     private String lastName;
+
+    @NotNull
+    @Email(message = "Invalid email address", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?``{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
+
+    @NotNull
+    @Size(min = 3, message = "Invalid password, minimum of 3 characters")
     private String password;
+
     private String confirmPassword;
 
     public String getFirstName() {
