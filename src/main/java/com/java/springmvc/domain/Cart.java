122c8cbaafb0354a98e37fc12a8a1e0380478872
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-05-03 06:26:46 +0700

    Add the Cart model and establish a one-to-one relationship with the User model and a one-to-many relationship with the CartDetail model

---
+++ src/main/java/com/java/springmvc/domain/Cart.java
@@ -0,0 +1,45 @@
+package com.java.springmvc.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long sum;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart")
+    private CartDetail cartDetail;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSum() {
+        return sum;
+    }
+
+    public void setSum(Long sum) {
+        this.sum = sum;
+    }
+
+}
